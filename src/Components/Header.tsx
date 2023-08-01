3bc822fb5c2667ca0620766d72a2b7569b8c23f5
preston176 <prestonnyamweya@gmail.com>
2023-08-01 18:48:39 +0300

    blogs pricing aboutus

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,9 +10,9 @@ const Header = () => {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#home">Blogs</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">About Us</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
