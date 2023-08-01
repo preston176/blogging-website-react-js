fd1e2d12b387c4acbc7301474faf5906149acc22
preston176 <prestonnyamweya@gmail.com>
2023-08-01 21:58:12 +0300

    write blog styled

--- src/Components/WriteBlog.tsx
+++ src/Components/WriteBlog.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const WriteBlog = () => {
   return (
     <div>
-      write blog
+        <Container>
+      <h2 className='text-center'>Do you want to sumbit your blog?</h2>
+      <form className='form-control d-flex flex-column shadow'>
+        <label htmlFor="#name">Enter your name</label>
+        <input className='my-2' type="text" placeholder='Your name ...' id='name' />
+        <label  htmlFor="#name">Blog title</label>
+        <input className='my-2' type="text" placeholder='Give the suitable heading for your blog...' id='name' />
+     <label htmlFor="img">Image link</label>
+      <input type="text" placeholder='image URL' id='img' className='my-2'/>
+      <textarea name="" id="" cols="30" rows="10" maxLength={100} placeholder='type here'></textarea>
+      <button className='btn btn-primary my-2'>Submit</button>
+      </form>
+      </Container>
     </div>
   )
 }
