1bbb2acdb597e74173cd75f610e99a0f1dd44064
preston176 <prestonnyamweya@gmail.com>
2023-08-02 01:40:07 +0300

    img breakpoints

--- BlogSphere-frontend/src/App.jsx
+++ BlogSphere-frontend/src/App.jsx
@@ -2,29 +2,30 @@
 import './App.css'
 
 import BlogsSection from './Components/BlogsSection'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Header from './Components/Header'
+
+
 import { useState, useEffect } from 'react';
-import WriteBlog from './Components/WriteBlog';
+
+import ToastNotification from './Components/ToastNotification';
 
 function App() {
   const [newsData, setNewsData] = useState([]);
 
   useEffect(() => {
-      fetch('https://whispering-falls-32785-62b22573fe66.herokuapp.com/api/newsData')
+    fetch('https://whispering-falls-32785-62b22573fe66.herokuapp.com/api/newsData')
       .then(Response => Response.json())
       .then(data => setNewsData(data))
 
   }, [])
-  
+
 
   return (
     <>
 
-      
-        {/* <Route path='/post' element={<WriteBlog />}></Route> */}
+      <ToastNotification />
+      {/* <Route path='/post' element={<WriteBlog />}></Route> */}
       <BlogsSection newsData={newsData} />
-      
+
     </>
   )
 }
