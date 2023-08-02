70816f530082f3da1efa745ce6cda409f5c9d3b9
preston176 <prestonnyamweya@gmail.com>
2023-08-02 04:16:03 +0300

    author fixed + scrollable right side

--- BlogSphere-frontend/src/Components/BlogsSection.jsx
+++ BlogSphere-frontend/src/Components/BlogsSection.jsx
@@ -2,35 +2,35 @@ import React from 'react'
 import BlogCard from './BlogCard'
 import { Container } from 'react-bootstrap'
 
-const BlogsSection = ({newsData}) => {
+const BlogsSection = ({ newsData }) => {
   return (
     <>
-    
-    <Container>
-    <h2>Recent Blog posts</h2>
-    <div className="blogs__Section">
-      <div className="left__Side">
-          {newsData.map(item => (
-            <BlogCard key={item.id} id={item.id} category={item.category} title={item.title} description={item.description} pic={item.imgURL} timestamp={item.timestamp}/> 
-          )) }
-        </div>
-       
-        <div className="right__Side">
-            <div className="right__Side__items">
-        <ul>      
+
+      <Container>
+        <h2>Recent Blog posts</h2>
+        <div className="blogs__Section">
+          <div className="left__Side">
             {newsData.map(item => (
-                 <li className='side__list'  key={item.id}>
-            <BlogCard id={item.id} category={item.category} title={item.title} pic={item.imgURL} timestamp={item.timestamp}/> 
-            </li>
-          )) }
-            </ul>
+              <BlogCard key={item.id} id={item.id} category={item.category} title={item.title} description={item.description} pic={item.imgURL} timestamp={item.timestamp} author={item.author} />
+            ))}
+          </div>
+
+          <div className="right__Side">
+            <div className="right__Side__items">
+              <ul>
+                {newsData.map(item => (
+                  <li className='side__list' key={item.id}>
+                    <BlogCard id={item.id} category={item.category} title={item.title} pic={item.imgURL} timestamp={item.timestamp} author={item.author} />
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-        </div>
-        </Container>
+      </Container>
     </>
   )
 }
 
 export default BlogsSection
-{/* */}
+{/* */ }
--- BlogSphere-frontend/src/main.jsx
+++ BlogSphere-frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import WriteBlog from './Components/WriteBlog.jsx';
